66175b47ef417488d5643d7f8e120123c27b4475
Edward GVH <155449554+EdwardVizcarra@users.noreply.github.com>
2024-01-23 23:41:04 +0000

    commit-portafolio-1

---
+++ portafolio/src/App.js
@@ -0,0 +1,75 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    
+    <div className="App">
+    
+<title>simply-portofolio-design</title>
+<div className="main">
+    <div className="cuadro-title">
+        <img className="image" src="assets/images/perfil_picture.jpeg" alt="jjj"/>
+        <div className="cuadro-cuadrito-title">
+        <h1>Victor Alvarado</h1>
+        <p>Software Designer</p>
+        <div className="icons">
+        <img src="assets/icons/github.svg" alt="github"/>
+        <img src="assets/icons/twitter.svg" alt="twitter"/>
+        <img src="assets/icons/linkedin.svg" alt="linkedin"/>
+        <img src="assets/icons/youtube.svg" alt="youtube"/>
+        </div>
+        </div>
+
+
+    </div>
+    <button className="boton" alert="contact me">Download</button>
+    <button className="boton-black" alert="contact me">Contact me</button>
+
+    <div className="cuadro-texto">
+        <section>
+            <div className="cuadro-cuadrito-texto">
+                <div className="cuadro-cuadrito-texto-title">
+                <h2>7</h2>
+                </div>
+                <div className="cuadro-cuadrito-texto-texto">
+                <p>Years of work experience
+                </p>
+                </div>
+                </div>
+                <div className="cuadro-cuadrito-texto">
+                    <div className="cuadro-cuadrito-texto-title">
+                    <h2>+50</h2>
+                    </div>
+                    <div className="cuadro-cuadrito-texto-texto">
+                    <p>Complete proyects
+                    </p>
+                    </div>
+                    </div>
+                    <div className="cuadro-cuadrito-texto">
+                        <div className="cuadro-cuadrito-texto-title">
+                        <h2>+20</h2>
+                        </div>
+                        <div className="cuadro-cuadrito-texto-texto">
+                        <p>Satisfied costumers
+                        </p>
+                        </div>
+                        </div>
+                    
+            </section>
+    </div>
+    <div className="container-image">
+        <img className="imagedown"src="assets/images/pictures_wall.jpeg" alt="1"/>  
+              <img className="imagedown"src="assets/images/bussiness_man.png" alt="1"/>
+        <img className="imagedown"src="assets/images/bussiness_man.png" alt="1"/>  
+        <img className="imagedown"src="assets/images/pictures_wall.jpeg" alt="1"/>
+                <img className="imagedown"src="assets/images/pictures_wall.jpeg" alt="1"/>  
+         <img className="imagedown"src="assets/images/bussiness_man.png" alt="1"/>
+    </div>
+</div>
+</div>
+
+  );
+};
+
+export default App;
